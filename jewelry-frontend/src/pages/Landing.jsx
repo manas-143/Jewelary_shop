cfb3ce9393f10507ff35545f25707d544cdb9dbc
abhishek-designs <akalyane980@gmail.com>
2025-12-07 13:31:22 +0530

    latest commit

--- jewelry-frontend/src/pages/Landing.jsx
+++ jewelry-frontend/src/pages/Landing.jsx
@@ -1,6 +1,30 @@
+import { use, useEffect, useState } from "react";
 import "./Landing.css";
+import { getProducts } from "../services/addProduct";
 
 function Landing() {
+  const [isLoading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const onFetchProducts = async () => {
+    try {
+      setLoading(true);
+      const data = await getProducts();
+      setProducts(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    onFetchProducts();
+  }, []);
+
+  useEffect(() => {
+    console.log(products, "------- products on landing page");
+  }, [products]);
+
   return (
     <div className="landing">
       <h1>Welcome to the Jewelry Shop</h1>
